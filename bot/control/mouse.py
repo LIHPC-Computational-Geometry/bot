7c33715608964cd007308d483ce312a17bcccf83
Ludofr3 <ludovic.de-chavagnac@epitech.eu>
2026-04-22 14:55:14 +0200

    refactor(mouse): modularize interaction logic and enable collision picking

--- bot/control/mouse.py
+++ bot/control/mouse.py
@@ -1,6 +1,6 @@
 from direct.showbase.InputStateGlobal import inputState
-from panda3d.core import Point2, MouseButton
-from panda3d.core import Point2, MouseButton, CollisionTraverser, CollisionNode, CollisionRay, CollisionHandlerQueue
+from panda3d.core import Point2, MouseButton, CollisionTraverser, CollisionNode, CollisionRay, CollisionHandlerQueue, GeomNode
+
 
 class MouseHandler:
     """
@@ -25,8 +25,8 @@ class MouseHandler:
         self.picker = CollisionTraverser()
         self.pq = CollisionHandlerQueue()
         self.pickerNode = CollisionNode('mouseRay')
-        self.pickerNode.setIntoCollideMask(0)
         self.pickerNP = self.base.camera.attachNewNode(self.pickerNode)
+        self.pickerNode.setIntoCollideMask(GeomNode.getDefaultCollideMask())
         self.pickerRay = CollisionRay()
         self.pickerNode.addSolid(self.pickerRay)
         self.picker.addCollider(self.pickerNP, self.pq)
@@ -38,6 +38,45 @@ class MouseHandler:
         self.base.accept("wheel_down", lambda: self.base.messenger.send("cmd_zoom", [1.1]))
 
         self.base.taskMgr.add(self.update, "MouseTask")
+
+    def _handle_hover(self, m_pos):
+        """Processes ray picking to detect hovered curves."""
+        self.pickerRay.setFromLens(self.base.camNode, m_pos.getX(), m_pos.getY())
+        self.picker.traverse(self.base.render)
+
+        hovered_tag = None
+        if self.pq.getNumEntries() > 0:
+            self.pq.sortEntries()
+            # On prend le premier objet touché par le rayon
+            entry = self.pq.getEntry(0)
+            np = entry.getIntoNodePath()
+            hovered_tag = np.getTag('curve_tag')
+
+        # Si on survole une nouvelle courbe (ou si on ne survole plus rien)
+        if hovered_tag != self.last_hovered_tag:
+            self.last_hovered_tag = hovered_tag
+            # On envoie l'info au parent via le callback
+            self.base._on_event_cb('hover', hovered_tag)
+
+    def _handle_drag(self, curr_pos):
+        """Handles mouse drag for rotating and panning."""
+        if self.base.mouseWatcherNode.isButtonDown(MouseButton.one()):
+            if self.prev_mouse_pos is not None:
+                # On calcule le delta par rapport à la frame précédente
+                delta = curr_pos - self.prev_mouse_pos
+
+                # On n'envoie le message QUE si la souris a réellement bougé
+                if delta.lengthSquared() > 0:
+                    if self.base.mouseWatcherNode.isButtonDown("shift"):
+                        self.base.messenger.send("cmd_pan", [delta.getX(), delta.getY()])
+                    else:
+                        self.base.messenger.send("cmd_rotate", [delta.getX(), delta.getY()])
+
+            # CRITIQUE : On met à jour prev_mouse_pos À CHAQUE FRAME
+            # pour que le delta reste minuscule entre deux frames.
+            self.prev_mouse_pos = Point2(curr_pos)
+        else:
+            self.prev_mouse_pos = None
 
     def update(self, task):
         """
@@ -54,40 +93,8 @@ class MouseHandler:
             m_pos = self.base.mouseWatcherNode.getMouse()
             curr_pos = Point2(m_pos.getX(), m_pos.getY())
 
-            self.pickerRay.setFromLens(self.base.camNode, m_pos.getX(), m_pos.getY())
-            self.picker.traverse(self.base.render)
-
-            hovered_tag = None
-            if self.pq.getNumEntries() > 0:
-                self.pq.sortEntries()
-                # On prend le premier objet touché par le rayon
-                entry = self.pq.getEntry(0)
-                np = entry.getIntoNodePath()
-                hovered_tag = np.getTag('curve_tag')
-
-            # Si on survole une nouvelle courbe (ou si on ne survole plus rien)
-            if hovered_tag != self.last_hovered_tag:
-                self.last_hovered_tag = hovered_tag
-                # On envoie l'info au parent via le callback
-                self.base._on_event_cb('hover', hovered_tag)
-
-            if self.base.mouseWatcherNode.isButtonDown(MouseButton.one()):
-                if self.prev_mouse_pos is not None:
-                    # On calcule le delta par rapport à la frame précédente
-                    delta = curr_pos - self.prev_mouse_pos
-
-                    # On n'envoie le message QUE si la souris a réellement bougé
-                    if delta.lengthSquared() > 0:
-                        if self.base.mouseWatcherNode.isButtonDown("shift"):
-                            self.base.messenger.send("cmd_pan", [delta.getX(), delta.getY()])
-                        else:
-                            self.base.messenger.send("cmd_rotate", [delta.getX(), delta.getY()])
-
-                # CRITIQUE : On met à jour prev_mouse_pos À CHAQUE FRAME
-                # pour que le delta reste minuscule entre deux frames.
-                self.prev_mouse_pos = Point2(curr_pos)
-            else:
-                self.prev_mouse_pos = None
+            self._handle_hover(m_pos)
+            self._handle_drag(curr_pos)
 
         return task.cont
 
